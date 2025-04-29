eac54c00ed15f9755b7772b090f5bb070e14d2d1
AlexandrSh <alexandr.shg01@yandex.ru>
2025-04-29 15:54:43 +0300

    5 sprint FL. Откорректирован согласно второго ревью.

--- src/com/yandex/app/Main.java
+++ src/com/yandex/app/Main.java
@@ -3,7 +3,6 @@ package com.yandex.app;
 import com.yandex.app.model.Epic;
 import com.yandex.app.model.Subtask;
 import com.yandex.app.model.Task;
-
 import com.yandex.app.service.Interfaces.TaskManager;
 import com.yandex.app.service.Managers;
 
